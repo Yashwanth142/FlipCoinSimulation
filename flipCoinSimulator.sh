009c83857a841e01470ad7920dca50b21f4a72f3
“Yashwanth142” <yashwanthvishnu14@gmail.com>
2022-12-13 18:32:16 +0530

    Added: Uc3 added

--- flipCoinSimulator.sh
+++ flipCoinSimulator.sh
@@ -1,7 +1,7 @@
 h=0
 t=0
 
-for (( i=1;i<20;i++ ))
+for (( i=1;i<42;i++ ))
 do
  c=$((RANDOM%2))
  if [[ $c -eq 0 ]]
@@ -10,5 +10,9 @@ do
  else
   t=$((t+1))
  fi
+ if [[ $h -eq 21 ||$t -eq 21 ]]
+ then
+  break
+ fi
 done
-echo "The number of times head is $h and number of times Tails is $t"
+echo "The number of times head is $h and number of times Tails is $t and differance is $(($h-$t))"
